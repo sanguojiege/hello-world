415d00a9362f135734a594e4cfb9fc8577f9b969
sanguojiege <714664495@qq.com>
2015-02-02 15:40:16 +0800

    this is test project

---
+++ 新建文本文档.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class Test {
+    public string id;
+    private ArrayList RankOpenList;
+    public override void Decode(string package) {
+        debug.log("ttt...");
+    }
+}
